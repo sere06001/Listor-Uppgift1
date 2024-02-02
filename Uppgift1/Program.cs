1aa7adb391435160b283283d68704790ffcbd9b5
sere06001 <sere06001@utb.vaxjo.se>
2024-02-02 12:24:33 +0100

    Add project files.

---
+++ Uppgift1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+namespace uppgift1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>();
+            Console.WriteLine("Skriv in ett eller flera namn, skriv en tom rad när du vill sluta.");
+            string svar = Console.ReadLine();
+            while (svar != "")
+            {
+                list.Add(svar);
+                svar = Console.ReadLine();
+            }
+            list.Sort();
+            list.Reverse();
+            foreach (string namn in list)
+            {
+                Console.WriteLine(namn);
+            }
+            Console.ReadKey();
+        }
+    }
+}
